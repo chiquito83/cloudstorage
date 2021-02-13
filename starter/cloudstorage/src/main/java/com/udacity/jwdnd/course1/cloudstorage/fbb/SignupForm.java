3338582081c3db9a0b1e40d7855fa9e8b5cea215
chiquito83 <adrian.grabowski@gmail.com>
2021-02-13 20:52:09 +0000

    add sign up form backing bean

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/fbb/SignupForm.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/fbb/SignupForm.java
@@ -1,4 +1,49 @@
 package com.udacity.jwdnd.course1.cloudstorage.fbb;
 
 public class SignupForm {
+
+  private String firstName;
+  private String lastName;
+
+  private String username;
+  private String password;
+
+  public SignupForm(String firstName, String lastName, String username, String password) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.username = username;
+    this.password = password;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
 }
