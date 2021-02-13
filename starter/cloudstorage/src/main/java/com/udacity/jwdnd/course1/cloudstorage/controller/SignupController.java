d5b9323efb67e913a23638baff1384bc7a4ce690
chiquito83 <adrian.grabowski@gmail.com>
2021-02-13 21:37:48 +0000

    signup controller

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -1,8 +1,10 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.fbb.SignupForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,14 +16,14 @@ public class SignupController {
   }
 
   @GetMapping
-  public String getSignupPage(Model model) {
+  public String getSignupPage(@ModelAttribute("signupForm") SignupForm signupForm, Model model) {
 
     return "signup";
 
   }
 
   @PostMapping
-  public String postSignupPage(Model model) {
+  public String postSignupPage(@ModelAttribute("signupForm") SignupForm signupForm, Model model) {
 
     return "signup";
   }
