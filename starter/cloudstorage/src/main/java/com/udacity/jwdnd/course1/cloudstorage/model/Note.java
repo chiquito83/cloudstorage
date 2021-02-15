f554ffdb5ff758bfe5f7ca06f4ce6f7979d11104
chiquito83 <adrian.grabowski@gmail.com>
2021-02-15 23:11:26 +0000

    add Note.java

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Note.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Note.java
@@ -1,5 +1,29 @@
 package com.udacity.jwdnd.course1.cloudstorage.model;
 
+
 public class Note {
 
+  private String title;
+  private String description;
+
+  public Note(String title, String description) {
+    this.title = title;
+    this.description = description;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  @Override
+  public String toString() {
+    return "Note{" +
+            "title='" + title + '\'' +
+            ", description='" + description + '\'' +
+            '}';
+  }
 }
