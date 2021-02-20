d5e8ebbe785c1987003ebee59a59fd774ae87017
chiquito83 <adrian.grabowski@gmail.com>
2021-02-20 17:09:29 +0000

    added logout

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/security/SecurityConfig.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/security/SecurityConfig.java
@@ -34,6 +34,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     http.formLogin()
             .defaultSuccessUrl("/home");
+
+    http.logout()
+            .logoutSuccessUrl("/home");
   }
 
 
